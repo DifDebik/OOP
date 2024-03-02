6e814ed232f58c0b7604405fa5b102fbcf2f232e
Dif Debik <zzeni42@gmail.com>
2024-03-03 00:37:28 +0300

    classmethod vs staticmethod

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     pay_rate = 0.8 # The pay rate after 20% discount
     all = []
@@ -21,19 +23,38 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name = item.get('name'),
+                price = float(item.get('price')),
+                quantity = int(item.get('quantity')),
+            )
+
+    @staticmethod
+    def is_integer(num):
+        if isinstance(num, float):
+            # Count out the decimal if it's .0
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
     def __repr__(self):
         return f'Item("{self.name}", {self.price}, {self.quantity})'
 
 
+Item.instantiate_from_csv()
 
-item1 = Item('Phone', 100, 1)
-item2 = Item('Laptop', 1000, 3)
-item3 = Item('Cable', 10, 5)
-item4 = Item('Mouse', 50, 5)
-item5 = Item('Keyboard', 75, 5)
+print(Item.is_integer(7.0))
 
-
-print(Item.all)
+# print(Item.all)
 
 # item1 = Item("Phone", 200, 5)
 # item1.apply_discount()
