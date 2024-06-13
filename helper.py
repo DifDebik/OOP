0bae6dd7c341ca0f7c74541ab07ae56aaf461cbf
Dif <zzeni42@gmail.com>
2024-06-13 20:06:08 +0300

    Inheritance

--- helper.py
+++ helper.py
@@ -2,7 +2,7 @@
 
 class Item:
     @staticmethod
-    def is_integer():
+    def is_integer(num):
         '''
         This should do something that has a relationship
         with the class, but not something that must be unique
